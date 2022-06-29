ead8c169813ffb22f481e02f3c19bc07630fa74c
Nguyễn Vũ Mạnh Hà <manhha584224@gmail.com>
2022-06-29 14:19:06 +0700

    update auth

--- routes/web.php
+++ routes/web.php
@@ -1,18 +1,29 @@
 <?php
 
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\LecturerController;
+use App\Http\Controllers\StudentController;
 use Illuminate\Support\Facades\Route;
-
-/*
-|--------------------------------------------------------------------------
-| Web Routes
-|--------------------------------------------------------------------------
-|
-| Here is where you can register web routes for your application. These
-| routes are loaded by the RouteServiceProvider within a group which
-| contains the "web" middleware group. Now create something great!
-|
-*/
+use Laravel\Socialite\Facades\Socialite;
 
 Route::get('/', function () {
-    return view('welcome');
-});
+    return view('layout/master');
+})->name('welcome');
+
+Route::get('login', [AuthController::class, 'login'])->name('login');
+Route::post('login', [AuthController::class, 'processLogin'])->name('process_login');
+
+Route::get('/register', [AuthController::class, 'register'])->name('register');
+Route::post('/register', [AuthController::class, 'registering'])->name('registering');
+
+Route::get('logout',[AuthController::class,'logout'])->name('logout');
+
+Route::get('/auth/redirect/{provider}', function ($provider) {
+    return Socialite::driver($provider)->redirect();
+})->name('auth.redirect');
+
+Route::get('/auth/callback/{provider}', [AuthController::class, 'callback'])->name('auth.callback');
+
+
+Route::resource('lecturers', LecturerController::class)->except(['show', 'destroy']);
+Route::resource('students', StudentController::class)->except(['show', 'destroy']);
