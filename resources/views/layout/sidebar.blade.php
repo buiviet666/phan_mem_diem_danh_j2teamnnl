f6743e266af0bbab83cf9eb069128fe6923f1d08
Nguyễn Vũ Mạnh Hà <manhha584224@gmail.com>
2022-07-23 00:03:51 +0700

    update paginate and ui

--- resources/views/layout/sidebar.blade.php
+++ resources/views/layout/sidebar.blade.php
@@ -11,56 +11,7 @@
     <!--- Sidemenu -->
     <ul class="metismenu side-nav">
 
-        @if (checkAdmin())
-            <li class="side-nav-item">
-                <a href="javascript: void(0);" class="side-nav-link">
-                    <i class="uil-home-alt"></i>
-                    <span>Trang chủ</span>
-                </a>
-            </li>
-
-            <li class="side-nav-item">
-                <a href="{{ route('admin_students') }}" class="side-nav-link">
-                    <i class="uil-home-alt"></i>
-                    <span>Quản lí sinh viên</span>
-                </a>
-            </li>
-
-            <li class="side-nav-item">
-                <a href="javascript: void(0);" class="side-nav-link">
-                    <i class="uil-home-alt"></i>
-                    <span>Quản lí giảng viên</span>
-                </a>
-            </li>
-            <li class="side-nav-item">
-                <a href="javascript: void(0);" class="side-nav-link">
-                    <i class="uil-home-alt"></i>
-                    <span>Quản lí lớp học</span>
-                </a>
-            </li>
-            <li class="side-nav-item">
-                <a href="javascript: void(0);" class="side-nav-link">
-                    <i class="uil-home-alt"></i>
-                    <span>Quản lí môn học</span>
-                </a>
-            </li>
-            <li class="side-nav-item">
-                <a href="{{ route('admin_departments') }}" class="side-nav-link">
-                    <i class="uil-home-alt"></i>
-                    <span>Quản lí khoa/ngành</span>
-                </a>
-            </li>
-            <li class="side-nav-item">
-                <a href="javascript: void(0);" class="side-nav-link">
-                    <i class="uil-home-alt"></i>
-                    <span>Phân công thời khóa biểu </span>
-                </a>
-            </li>
-        @endif
-
-
-
-
+       @yield('menusidebar')
 
     </ul>
     <!-- End Sidebar -->
